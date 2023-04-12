b2ef8d7f1a4de93ec8d6220f59df23d0ba8c63a6
Jan Kvetina <jan.kvetina@gmail.com>
2023-04-12 16:07:49 +0200

    Move board logic into package

--- database/packages/tsk_app.spec.sql
+++ database/packages/tsk_app.spec.sql
@@ -6,6 +6,15 @@ CREATE OR REPLACE PACKAGE tsk_app AS
 
     PROCEDURE load_tasks_filters;
 
+
+
+    PROCEDURE generate_board (
+        in_client_id        tsk_boards.client_id%TYPE           := NULL,
+        in_project_id       tsk_boards.project_id%TYPE          := NULL,
+        in_board_id         tsk_boards.board_id%TYPE            := NULL,
+        in_swimlane_id      tsk_task_swimlanes.swimlane_id%TYPE := NULL
+    );
+
 END;
 /
 
--- database/packages/tsk_app.sql
+++ database/packages/tsk_app.sql
@@ -69,6 +69,113 @@ CREATE OR REPLACE PACKAGE BODY tsk_app AS
 
         -- also save them, they might be changed without submitting the page
         tsk_app.save_tasks_filters();
+
+
+    PROCEDURE generate_board (
+        in_client_id        tsk_boards.client_id%TYPE           := NULL,
+        in_project_id       tsk_boards.project_id%TYPE          := NULL,
+        in_board_id         tsk_boards.board_id%TYPE            := NULL,
+        in_swimlane_id      tsk_task_swimlanes.swimlane_id%TYPE := NULL
+    )
+    AS
+        v_statuses     PLS_INTEGER;
+    BEGIN
+        -- calculate number of columns
+        SELECT COUNT(s.status_id)
+        INTO v_statuses
+        FROM tsk_task_statuses s
+        WHERE s.client_id       = in_client_id
+            AND s.project_id    = in_project_id
+            AND s.is_active     = 'Y';
+        --
+        HTP.P('<div class="BOARD" style="' ||
+            'grid-template-columns: repeat(' || v_statuses || ', minmax(300px, 1fr)); ' ||
+            '">');
+
+        -- generate grid
+        FOR w IN (
+            SELECT
+                w.*,
+                ROW_NUMBER() OVER (ORDER BY CASE WHEN w.swimlane_id = '-' THEN NULL ELSE w.order# END NULLS LAST) AS r#
+            FROM tsk_task_swimlanes w
+            WHERE w.client_id       = in_client_id
+                AND w.project_id    = in_project_id
+                AND (w.swimlane_id  = in_swimlane_id OR in_swimlane_id IS NULL)
+                AND w.is_active     = 'Y'
+            ORDER BY r#
+        ) LOOP
+            -- create column names
+            FOR s IN (
+                SELECT
+                    s.status_id,
+                    s.status_name,
+                    ROWNUM AS r#
+                FROM tsk_task_statuses s
+                WHERE s.client_id       = in_client_id
+                    AND s.project_id    = in_project_id
+                    AND s.is_active     = 'Y'
+                ORDER BY s.order#
+            ) LOOP
+                HTP.P('<div class="TARGET_LIKE">');
+                HTP.P('<h3>' || s.status_name || CASE WHEN s.r# = 1 THEN NULLIF(' @' || w.swimlane_name, ' @-') END || '</h3>');
+                HTP.P('</div>');
+            END LOOP;
+
+            -- create swimlanes
+            FOR s IN (
+                SELECT
+                    s.status_id
+                FROM tsk_task_statuses s
+                WHERE s.client_id       = in_client_id
+                    AND s.project_id    = in_project_id
+                    AND s.is_active     = 'Y'
+                ORDER BY s.order#
+            ) LOOP
+                HTP.P('<div class="TARGET" id="STATUS_' || s.status_id || '_SWIMLANE_' || w.swimlane_id || '">');
+                --
+                FOR t IN (
+                    SELECT
+                        t.task_id,
+                        t.task_name,
+                        --
+                        APEX_PAGE.GET_URL (
+                            p_page          => 105,
+                            p_clear_cache   => 105,
+                            p_items         => 'P105_TASK_ID,P105_SOURCE_PAGE',
+                            p_values        => '' || t.task_id || ',100'
+                        ) AS task_link,
+                        --
+                        NULLIF(SUM(CASE WHEN l.checklist_done = 'Y' THEN 1 ELSE 0 END) || '/' || COUNT(l.checklist_id), '0/0') AS task_progress
+                    FROM tsk_tasks t
+                    LEFT JOIN tsk_task_checklist l
+                        ON l.task_id        = t.task_id
+                    WHERE t.client_id       = in_client_id
+                        AND t.project_id    = in_project_id
+                        AND t.board_id      = in_board_id
+                        AND t.status_id     = s.status_id
+                        AND t.swimlane_id   = w.swimlane_id
+                    GROUP BY
+                        t.task_id,
+                        t.task_name,
+                        t.order#
+                    ORDER BY t.order# NULLS LAST, t.task_id
+                ) LOOP
+                    HTP.P(
+                        '<div class="TASK" draggable="true" id="TASK_' || t.task_id || '">' ||
+                        '<a href="' || t.task_link || '">' ||
+                        CASE WHEN t.task_progress IS NOT NULL
+                            THEN '<span style="float: right;">' || t.task_progress || '</span>'
+                            END ||
+                        '#' || t.task_id || ' ' || t.task_name ||
+                        '</a></div>'
+                    );
+                END LOOP;
+                --
+                HTP.P('</div>');
+            END LOOP;
+        END LOOP;
+        --
+        HTP.P('</div>');
     END;
 
 END;
