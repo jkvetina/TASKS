de6e68095f66ad1f554a44b4276b14685331c036
Jan Kvetina <jan.kvetina@gmail.com>
2023-04-14 22:43:00 +0200

    Fix sorting on prev/next buttons

--- database/views/tsk_p100_tasks_grid_v.sql
+++ database/views/tsk_p100_tasks_grid_v.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE FORCE VIEW tsk_p100_tasks_grid_v AS
 WITH x AS (
     SELECT /*+ MATERIALIZE */
-        core.get_item('P0_CLIENT_ID')   AS client_id,
-        core.get_item('P0_PROJECT_ID')  AS project_id,
-        core.get_item('P0_BOARD_ID')    AS board_id
+        core.get_item('P0_CLIENT_ID')           AS client_id,
+        core.get_item('P0_PROJECT_ID')          AS project_id,
+        core.get_number_item('P0_BOARD_ID')     AS board_id
     FROM DUAL
 )
 SELECT
@@ -20,10 +20,17 @@ SELECT
     NULLIF(SUM(CASE WHEN l.checklist_done = 'Y' THEN 1 ELSE 0 END) || '/' || COUNT(l.checklist_id), '0/0') AS task_progress,
     --
     t.status_id,
-    t.swimlane_id
+    t.swimlane_id,
+    w.order#            AS swimlane_order#,
+    s.order#            AS status_order#,
+    t.order#            AS task_order#
     --
 FROM tsk_tasks t
 CROSS JOIN x
+JOIN tsk_task_statuses s
+    ON s.status_id      = t.status_id
+JOIN tsk_task_swimlanes w
+    ON w.swimlane_id    = t.swimlane_id
 LEFT JOIN tsk_task_checklist l
     ON l.task_id        = t.task_id
 WHERE t.client_id       = x.client_id
@@ -35,6 +42,8 @@ GROUP BY
     t.task_name,
     t.status_id,
     t.swimlane_id,
+    w.order#,
+    s.order#,
     t.order#;
 --
 COMMENT ON TABLE tsk_p100_tasks_grid_v IS '';
