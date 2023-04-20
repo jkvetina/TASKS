ee243c70897ffd02133d2d2fe5cc5f4da845a75c
Jan Kvetina <jan.kvetina@gmail.com>
2023-04-20 23:38:26 +0200

    Favorite boards table

---
+++ database/tables/tsk_user_fav_boards.sql
@@ -0,0 +1,23 @@
+CREATE TABLE tsk_user_fav_boards (
+    user_id                         VARCHAR2(128)   CONSTRAINT nn_tsk_user_fav_boards_user NOT NULL,
+    board_id                        NUMBER(10,0)    CONSTRAINT nn_tsk_user_fav_boards_id NOT NULL,
+    updated_by                      VARCHAR2(128),
+    updated_at                      DATE,
+    --
+    CONSTRAINT pk_tsk_user_fav_boards
+        PRIMARY KEY (user_id, board_id),
+    --
+    CONSTRAINT fk_tsk_user_fav_boards_board
+        FOREIGN KEY (board_id)
+        REFERENCES tsk_boards (board_id),
+    --
+    CONSTRAINT fk_tsk_user_fav_boards_user
+        FOREIGN KEY (user_id)
+        REFERENCES tsk_users (user_id)
+);
+--
+COMMENT ON TABLE tsk_user_fav_boards IS '';
+--
+COMMENT ON COLUMN tsk_user_fav_boards.user_id       IS '';
+COMMENT ON COLUMN tsk_user_fav_boards.board_id      IS '';
+
