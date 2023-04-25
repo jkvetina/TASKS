a9ed7dbf0dec205435a1bc9e515fb9e60fbfa0d2
Jan Kvetina <jan.kvetina@gmail.com>
2023-04-25 19:12:24 +0200

    Default roles

---
+++ patches/60_data/tsk_auth_pages.sql
@@ -0,0 +1,55 @@
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--');
+    DBMS_OUTPUT.PUT_LINE('-- MERGE ' || UPPER('tsk_auth_pages'));
+    DBMS_OUTPUT.PUT_LINE('--');
+END;
+/
+--
+DELETE FROM tsk_auth_pages;
+--
+MERGE INTO tsk_auth_pages t
+USING (
+    SELECT 'BOARDS_MANAGER' AS role_id, 200 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'BOARDS_MANAGER' AS role_id, 205 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 100 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 105 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 106 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 110 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 115 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 200 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 300 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 500 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 900 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 980 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 200 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 205 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 230 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 250 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 270 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 300 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 510 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 515 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'USERS_MANAGER' AS role_id, 300 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'USERS_MANAGER' AS role_id, 400 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'USERS_MANAGER' AS role_id, 950 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'USERS_MANAGER' AS role_id, 952 AS page_id, 'Y' AS is_active FROM DUAL UNION ALL
+    SELECT 'USERS_MANAGER' AS role_id, 958 AS page_id, 'Y' AS is_active FROM DUAL
+) s
+ON (
+    t.role_id = s.role_id
+    AND t.page_id = s.page_id
+)
+--WHEN MATCHED THEN
+--    UPDATE SET
+--        t.is_active = s.is_active
+WHEN NOT MATCHED THEN
+    INSERT (
+        t.role_id,
+        t.page_id,
+        t.is_active
+    )
+    VALUES (
+        s.role_id,
+        s.page_id,
+        s.is_active
+    );
---
+++ patches/60_data/tsk_roles.sql
@@ -0,0 +1,43 @@
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--');
+    DBMS_OUTPUT.PUT_LINE('-- MERGE ' || UPPER('tsk_roles'));
+    DBMS_OUTPUT.PUT_LINE('--');
+END;
+/
+--
+DELETE FROM tsk_roles;
+--
+MERGE INTO tsk_roles t
+USING (
+    SELECT 'BOARDS_MANAGER' AS role_id, 'Boards Manager' AS role_name, 'MANAGEMENT' AS role_group, 'Create/update/delete Boards' AS role_desc, 'Y' AS is_active, 10 AS order# FROM DUAL UNION ALL
+    SELECT 'DEFAULT_USER' AS role_id, 'Default User' AS role_name, 'TASKS' AS role_group, 'Needed so user can access and use the app (create/update tasks)' AS role_desc, 'Y' AS is_active, 10 AS order# FROM DUAL UNION ALL
+    SELECT 'PROJECT_MANAGER' AS role_id, 'Project Manager' AS role_name, 'MANAGEMENT' AS role_group, 'Create/update/delete Projects, Statuses, Swimlanes, Sequences' AS role_desc, 'Y' AS is_active, 10 AS order# FROM DUAL UNION ALL
+    SELECT 'USERS_MANAGER' AS role_id, 'Users Manager' AS role_name, 'MANAGEMENT' AS role_group, 'Can add Users to Customers, assign Roles to them' AS role_desc, 'Y' AS is_active, NULL AS order# FROM DUAL
+) s
+ON (
+    t.role_id = s.role_id
+)
+--WHEN MATCHED THEN
+--    UPDATE SET
+--        t.role_name = s.role_name,
+--        t.role_group = s.role_group,
+--        t.role_desc = s.role_desc,
+--        t.is_active = s.is_active,
+--        t.order# = s.order#
+WHEN NOT MATCHED THEN
+    INSERT (
+        t.role_id,
+        t.role_name,
+        t.role_group,
+        t.role_desc,
+        t.is_active,
+        t.order#
+    )
+    VALUES (
+        s.role_id,
+        s.role_name,
+        s.role_group,
+        s.role_desc,
+        s.is_active,
+        s.order#
+    );
