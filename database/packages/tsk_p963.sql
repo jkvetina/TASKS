e9c9363dee9297eeb8cd4dc838d8a2e268ce7802
Jan Kvetina <jan.kvetina@gmail.com>
2023-05-05 19:09:37 +0200

    Show not assigned rows in header

--- database/packages/tsk_p963.sql
+++ database/packages/tsk_p963.sql
@@ -7,7 +7,7 @@ CREATE OR REPLACE PACKAGE BODY tsk_p963 AS
         tsk_p960.set_role_names();
 
         -- notify for new components
-        SELECT NULLIF(' (' || COUNT(*) || ')', ' (0)') INTO v_not_assigned
+        SELECT NULLIF('(' || COUNT(*) || ')', '(0)') INTO v_not_assigned
         FROM tsk_p963_map_components_v c
         WHERE c.is_used IS NULL;
         --
