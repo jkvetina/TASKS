07d36ff5cff91495c6407ed4631cc94a20e28792
Jan Kvetina <jan.kvetina@gmail.com>
2023-05-13 07:57:27 +0200

    Show more stats/counts on Projects

--- database/views/tsk_p300_projects_v.sql
+++ database/views/tsk_p300_projects_v.sql
@@ -5,7 +5,7 @@ WITH x AS (
         tsk_app.get_project_id()    AS project_id
     FROM DUAL
 ),
-c AS (
+b AS (
     SELECT /*+ MATERIALIZE */
         t.project_id,
         --
@@ -13,6 +13,39 @@ c AS (
         COUNT(*)                    AS count_tasks
         --
     FROM tsk_tasks t
+    JOIN x
+        ON x.client_id  = t.client_id
+    GROUP BY
+        t.project_id
+),
+s AS (
+    SELECT /*+ MATERIALIZE */
+        t.project_id,
+        --
+        COUNT(*)        AS count_statuses
+    FROM tsk_statuses t
+    JOIN x
+        ON x.client_id  = t.client_id
+    GROUP BY
+        t.project_id
+),
+w AS (
+    SELECT /*+ MATERIALIZE */
+        t.project_id,
+        --
+        COUNT(*)        AS count_swimlanes
+    FROM tsk_swimlanes t
+    JOIN x
+        ON x.client_id  = t.client_id
+    GROUP BY
+        t.project_id
+),
+c AS (
+    SELECT /*+ MATERIALIZE */
+        t.project_id,
+        --
+        COUNT(*)        AS count_categories
+    FROM tsk_categories t
     JOIN x
         ON x.client_id  = t.client_id
     GROUP BY
@@ -39,14 +72,20 @@ SELECT
     --
     t.is_active,
     --
-    c.count_boards,
-    c.count_tasks
+    c.count_categories,
+    s.count_statuses,
+    w.count_swimlanes,
+    b.count_boards,
+    b.count_tasks
     --
 FROM tsk_projects t
 JOIN x
-    ON x.client_id      = t.client_id
-LEFT JOIN c
-    ON c.project_id     = t.project_id;
+    ON x.client_id          = t.client_id
+    --
+LEFT JOIN b ON b.project_id = t.project_id
+LEFT JOIN s ON s.project_id = t.project_id
+LEFT JOIN w ON w.project_id = t.project_id
+LEFT JOIN c ON c.project_id = t.project_id;
 --
 COMMENT ON TABLE tsk_p300_projects_v IS '';
 
