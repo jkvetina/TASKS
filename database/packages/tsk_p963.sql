6223fcafa2cfe7071884064b6d528c001cd7e457
Jan Kvetina <jan.kvetina@gmail.com>
2023-05-29 21:41:53 +0200

    Discover components upfront

--- database/packages/tsk_p963.sql
+++ database/packages/tsk_p963.sql
@@ -33,16 +33,89 @@ CREATE OR REPLACE PACKAGE BODY tsk_p963 AS
 
     PROCEDURE refresh_mv
     AS
+        v_count     PLS_INTEGER;
+        v_cols      VARCHAR2(4000);
     BEGIN
-        -- remove components which dont exists on page anymore
-        DELETE FROM tsk_auth_components a
-        WHERE a.component_id IN (
-            SELECT a.component_id
-            FROM tsk_auth_components a
-            JOIN tsk_p963_map_components_v m
-                ON m.component_id   = a.component_id
-            WHERE m.dml_actions     NOT LIKE '%U%'
+        -- go through all APEX view and find out components with authorization_scheme
+        FOR c IN (
+            SELECT
+                d.apex_view_name        AS table_name,
+                MAX(n.comments)         AS comments
+            FROM apex_dictionary d
+            JOIN apex_dictionary n
+                ON n.apex_view_name     = d.apex_view_name
+                AND n.column_id         = 0
+            WHERE d.column_name         IN ('AUTHORIZATION_SCHEME_ID', 'APPLICATION_ID')
+                AND d.apex_view_name    NOT IN (
+                    'APEX_APPLICATIONS',
+                    'APEX_APPLICATION_AUTHORIZATION',
+                    'APEX_APPLICATION_ALL_AUTH',
+                    'APEX_APPLICATION_PAGES',
+                    'APEX_DEBUG_MESSAGES',
+                    'APEX_WORKSPACE_ACTIVITY_LOG'
+                )
+            GROUP BY d.apex_view_name
+            HAVING COUNT(*) = 2
+            ORDER BY 1
+        ) LOOP
+            -- add missing components
+            BEGIN
+                EXECUTE IMMEDIATE
+                    'SELECT COUNT(*) FROM ' || c.table_name
+                    || ' WHERE application_id = :app_id'
+                    || ' AND authorization_scheme_id IS NOT NULL'
+                    INTO v_count
+                    USING IN core.get_app_id();
+                --
+                IF v_count > 0 THEN
+                    DBMS_OUTPUT.PUT_LINE(c.table_name || ' = ' || v_count);
+                    --
+                    FOR d IN (
+                        SELECT t.component_id, t.page_id
+                        FROM tsk_auth_components_list_v t
+                        WHERE t.component_type          = c.table_name
+                            AND authorization_scheme_id IS NOT NULL
+                    ) LOOP
+                        DBMS_OUTPUT.PUT_LINE('    ' || d.component_id);
+                        --
+                        BEGIN
+                            INSERT INTO tsk_auth_components (component_id, component_type, component_name, page_id, is_active)
+                            VALUES (
+                                d.component_id,
+                                c.table_name,
+                                '?',
+                                d.page_id,
+                                'Y'
+                            );
+                        EXCEPTION
+                        WHEN DUP_VAL_ON_INDEX THEN
+                            NULL;
+                        END;
+                    END LOOP;
+                    --
+                    DBMS_OUTPUT.PUT_LINE('    --');
+                END IF;
+            EXCEPTION
+            WHEN OTHERS THEN
+                DBMS_OUTPUT.PUT_LINE('ERROR:' || c.table_name);
+                RAISE;
+            END;
+        END LOOP;
+
+        -- remove obsolete components
+        DELETE FROM tsk_auth_components c
+        WHERE c.component_id IN (
+            SELECT c.component_id
+            FROM tsk_auth_components c
+            LEFT JOIN tsk_auth_components_list_v t
+                ON t.component_id   = c.component_id
+            WHERE t.component_id    IS NULL
         );
+
+        -- update/shorten component type
+        UPDATE tsk_auth_components c
+        SET c.component_type    = tsk_auth.get_component_type(c.component_type)    
+        WHERE c.component_type != tsk_auth.get_component_type(c.component_type);
 
         -- refresh regions
         DBMS_MVIEW.REFRESH('TSK_P963_REGIONS_MV', method => 'C');
