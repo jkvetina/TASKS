b4f8e980c40241bdda6a1e84b7454eead7495bfc
Jan Kvetina <jan.kvetina@gmail.com>
2023-08-01 20:27:09 +0200

    Cleanup

--- database/apex/f710/application/pages/page_00000.sql
+++ database/apex/f710/application/pages/page_00000.sql
@@ -4,8 +4,8 @@ begin
 --     PAGE: 00000
 --   Manifest End
 wwv_flow_imp.component_begin (
- p_version_yyyy_mm_dd=>'2022.10.07'
-,p_release=>'22.2.4'
+ p_version_yyyy_mm_dd=>'2023.04.28'
+,p_release=>'23.1.0'
 ,p_default_workspace_id=>8506563800894011
 ,p_default_application_id=>710
 ,p_default_id_offset=>84847035874187356
@@ -30,7 +30,6 @@ wwv_flow_imp_page.create_page_plug(
 ,p_plug_display_sequence=>30
 ,p_include_in_reg_disp_sel_yn=>'Y'
 ,p_plug_display_point=>'BEFORE_FOOTER'
-,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
 ,p_attribute_01=>'N'
 ,p_attribute_02=>'HTML'
 );
@@ -44,45 +43,8 @@ wwv_flow_imp_page.create_page_plug(
 ,p_plug_display_point=>'BEFORE_FOOTER'
 ,p_plug_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
 '<script>',
-'',
-'//',
-'// WAIT FOR ELEMENT TO EXIST',
-'//',
-'var wait_for_element = function(search, start, fn, disconnect) {',
-'    var ob  = new MutationObserver(function(mutations) {',
-'        if ($(search).length) {',
-'            fn(search, start);',
-'            if (disconnect) {',
-'                observer.disconnect();  // keep observing',
-'            }',
-'        }',
-'    });',
-'    //',
-'    ob.observe(document.getElementById(start), {',
-'        childList: true,',
-'        subtree: true',
-'    });',
-'};',
-'',
-'//',
-'// HIDE SUCCESS MESSAGE',
-'//',
-'var hide_success_message = function(search, start) {',
-'    var $start = $(''#'' + start);',
-'    //',
-'    setTimeout(function() {',
-'        apex.message.hidePageSuccess();  // hide message',
-'        var content = $start.text().trim();',
-'        if (content.length) {',
-'            console.log(''MESSAGE CLOSED:'', content);',
-'        }',
-'        $start.html('''').removeClass(''u-visible'');  // clean APEX mess',
-'    }, 2000);',
-'};',
-'',
 '</script>',
 ''))
-,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
 ,p_attribute_01=>'N'
 ,p_attribute_02=>'HTML'
 );
@@ -109,7 +71,6 @@ wwv_flow_imp_page.create_page_plug(
 '',
 '</style>',
 ''))
-,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
 ,p_attribute_01=>'N'
 ,p_attribute_02=>'HTML'
 );
